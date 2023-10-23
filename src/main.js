2b5151f2cd236de958e45b625c16f7c64e688a0e
Matheus Fré <matheusfre94@gmail.com>
2023-10-23 16:07:46 -0300

    criando o header

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll("[data-tab-button]");
     const questions = document.querySelectorAll("[data-faq-question]");
 
+    const hero_section = document.querySelector(".hero");
+    const altura_hero = hero_section.clientHeight;
+
+    window.addEventListener("scroll", function() {
+        const posicao_atual = window.scrollY;
+
+        if (posicao_atual < altura_hero) {
+            oculta_elementos();
+        } else {
+            exibe_elementos();
+        }
+    })
+
+    // Seção Atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function(botao) {
             const aba_alvo = botao.target.dataset.tabButton;
@@ -13,10 +27,21 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    // Seção FAQ, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener("click", abreOuFechaResposta);
     }
 })
+
+function oculta_elementos() {
+    const header = document.querySelector("header");
+    header.classList.add("header--is-hidden");
+}
+
+function exibe_elementos() {
+    const header = document.querySelector("header");
+    header.classList.remove("header--is-hidden");
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = "faq__questions__item--is-open";
